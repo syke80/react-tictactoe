45e8a545f1cb034bd6b6ff39d6246185836a10da
Attila Keszthelyi <attilakeszthelyi@Attilas-MacBook-Pro.local>
2018-07-18 15:18:12 +0200

    split components to multiple files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from './App';
+import Game from './Game';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
